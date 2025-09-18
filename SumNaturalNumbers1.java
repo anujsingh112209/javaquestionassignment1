0cf8d72f6c6dfd1eca9a57c41065fb82c1507be2
anujsingh112209 <singhanuj4735@gmail.com>
2025-09-18 21:53:23 +0530

    Add files via upload

---
+++ SumNaturalNumbers1.java
@@ -0,0 +1,13 @@
+public class SumNaturalNumbers1 {
+static int sum(int n) {
+        if (n == 0) {
+            return 0;   
+        }
+        return n + sum(n - 1);  
+    }
+
+    public static void main(String[] args) {
+        int n = 5; 
+        System.out.println("Sum of numbers from " + n + " to 1 = " + sum(n));
+    }
+}
