d50ed618d88963889f24a6a4f4763a3e86dd4b06
anujsingh112209 <singhanuj4735@gmail.com>
2025-09-18 20:16:57 +0530

    Add files via upload

---
+++ SumOfDigitsRecursion.java
@@ -0,0 +1,24 @@
+
+    import java.util.Scanner;
+
+public class SumOfDigitsRecursion {
+    
+    static int sumOfDigits(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return (n % 10) + sumOfDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int result = sumOfDigits(num);
+        System.out.println("Sum of digits = " + result);
+
+        sc.close();
+    }
+}
